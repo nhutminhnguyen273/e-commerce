7908fedad030bcc19a66e0c5e592c5e1f111ea4b
nhutminhnguyen273 <nhutminhnguyen293@gmail.com>
2025-02-16 19:33:14 +0700

    first commit

---
+++ voucher-service/src/main/java/com/example/voucher_service/VoucherServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.voucher_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoucherServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoucherServiceApplication.class, args);
+	}
+
+}
---
+++ voucher-service/src/test/java/com/example/voucher_service/VoucherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.voucher_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
